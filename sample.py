c74ca81fbff880bbfc0ea5a9d40455dc9f549fcb
arhvel <v.e.adeyemo@gmail.com>
2023-11-27 10:49:55 +0000

    edited for pushshift

--- sample.py
+++ sample.py
@@ -6,144 +6,76 @@ Created on Mon Nov 27 10:13:06 2023
 """
 
 import requests
-from bs4 import BeautifulSoup
-urlFilm = "https://quotes.toscrape.com/author/Marilyn-Monroe/"
-page = requests.get(urlFilm)
-soup = BeautifulSoup(page.content, "html.parser")
-soup
+import json
+import re
+import time
 
+PUSHSHIFT_REDDIT_URL = "http://api.pushshift.io/reddit"
 
-name = soup.find("h3", class_="author-title")
-name.text
+def fetchObjects(**kwargs):
+    # Default paramaters for API query
+    params = {
+        "sort_type":"created_utc",
+        "sort":"asc",
+        "size":1000
+        }
 
-spanBorn = soup.find("span", class_="author-born-date")
-print(spanBorn.text)
+    # Add additional paramters based on function arguments
+    for key,value in kwargs.items():
+        params[key] = value
 
-bornLocation = soup.find("span", class_ ="author-born-location")
-bornLocation.text
+    # Print API query paramaters
+    print(params)
 
-description = soup.find("div", class_ = "author-description")
-description.text
-
-
-urlFilm = "https://quotes.toscrape.com/author/Jane-Austen/"
-page = requests.get(urlFilm)
-soup = BeautifulSoup(page.content, "html.parser")
-soup
-
-
-name = soup.find("h3", class_="author-title")
-print(name.text)
-
-spanBorn = soup.find("span", class_="author-born-date")
-print(spanBorn.text)
-
-bornLocation = soup.find("span", class_ ="author-born-location")
-bornLocation.text
-
-description = soup.find("div", class_ = "author-description")
-description.text
-
-
-
-## Data Munging
-
-### date formatting
-from datetime import datetime
-
-dateFormat = '%B %d %Y'
-
-u = list(spanBorn.text)
-u.remove(",")
-date = ''.join(e for e in u)
-date
-
-dateObject = datetime.strptime(spanBorn.text,dateFormat)
-
-
-### Location formatting
-
-bornLocation.text
-
-twc = bornLocation.text.replace('in',"").split(",")
-Country = twc[2]
-City = twc[1]
-Town = twc[0]
-
-
-## Data Analysis
-import requests
-import datetime
-from bs4 import BeautifulSoup
-
-
-# Storing the URLs for different authors
-urls = []
-urls.append("https://quotes.toscrape.com/author/J-K-Rowling/")
-urls.append("http://quotes.toscrape.com/author/Jane-Austen/")
-urls.append("https://quotes.toscrape.com/author/Albert-Einstein/")
-urls.append("http://quotes.toscrape.com/author/Marilyn-Monroe/")
-urls.append("http://quotes.toscrape.com/author/Andre-Gide/")
-urls.append("http://quotes.toscrape.com/author/Thomas-A-Edison/")
-urls.append("http://quotes.toscrape.com/author/Eleanor-Roosevelt/")
-urls.append("http://quotes.toscrape.com/author/Steve-Martin/")
-urls.append("http://quotes.toscrape.com/author/Bob-Marley/")
-urls.append("http://quotes.toscrape.com/author/Dr-Seuss/")
-urls.append("http://quotes.toscrape.com/author/Douglas-Adams/")
-urls.append("http://quotes.toscrape.com/author/Friedrich-Nietzsche/")
-urls.append("http://quotes.toscrape.com/author/Mark-Twain/")
-urls.append("http://quotes.toscrape.com/author/Pablo-Neruda/")
-urls.append("http://quotes.toscrape.com/author/Ralph-Waldo-Emerson/")
-urls.append("http://quotes.toscrape.com/author/Mother-Teresa/")
-urls.append("http://quotes.toscrape.com/author/Garrison-Keillor/")
-urls.append("http://quotes.toscrape.com/author/Charles-M-Schulz/")
-urls.append("http://quotes.toscrape.com/author/William-Nicholson/")
-
-
-# Looping through all the pages
-authors = []
-for url in urls:
-
-    # Get the page
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    # Set the type variable based on function input
+    # The type can be "comment" or "submission", default is "comment"
+    type = "comment"
+    if 'type' in kwargs and kwargs['type'].lower() == "submission":
+        type = "submission"
     
-    # Get the information
-    spanBorn = soup.find("span", class_="author-born-date")
-    spanLocation = soup.find("span", class_="author-born-location")
-    divDescription = soup.find("div", class_="author-description")
-    h3AuthorTitle = soup.find("h3", class_="author-title")  
-    if (h3AuthorTitle.text.find("\n")!=-1):
-        strAuthorTitle = h3AuthorTitle.text[:h3AuthorTitle.text.find("\n")] # Just take the first line
-    else:
-        strAuthorTitle = h3AuthorTitle.text
+    # Perform an API request
+    r = requests.get(PUSHSHIFT_REDDIT_URL + "/" + type + "/search/", params=params, timeout=30)
+
+    # Check the status code, if successful, process the data
+    if r.status_code == 200:
+        response = json.loads(r.text)
+        data = response['data']
+        sorted_data_by_id = sorted(data, key=lambda x: int(x['id'],36))
+        return sorted_data_by_id
+
+def extract_reddit_data(**kwargs):
+    # Speficify the start timestamp
+    max_created_utc = 1356998400  # 01/01/2013 @ 12:00am (UTC)
+    max_id = 0
+
+    # Open a file for JSON output
+    file = open("submissions.json","a")
+
+    # While loop for recursive function
+    while 1:
+        nothing_processed = True
+        # Call the recursive function
+        objects = fetchObjects(**kwargs,after=max_created_utc)
         
-    # Munging for born date
-    strBornDate = spanBorn.text
-    dateTimeObject = datetime.datetime.strptime(strBornDate, '%B %d, %Y')
+        # Loop the returned data, ordered by date
+        for object in objects:
+            id = int(object['id'],36)
+            if id > max_id:
+                nothing_processed = False
+                created_utc = object['created_utc']
+                max_id = id
+                if created_utc > max_created_utc: max_created_utc = created_utc
+                # Output JSON data to the opened file
+                print(json.dumps(object,sort_keys=True,ensure_ascii=True),file=file)
+        
+        # Exit if nothing happened
+        if nothing_processed: return
+        max_created_utc -= 1
 
-    # Munging for born place
-    strBornLocation = spanLocation.text
-    strBornLocation = strBornLocation.replace("in ", "")
-    iLastComma = strBornLocation.rfind(",")
-    if (iLastComma == -1):
-        strCountry = strBornLocation
-        strTownState = ""
-    else:
-        strCountry = strBornLocation[iLastComma+2:]
-        strTownState = strBornLocation[:iLastComma]
-    iSecondComma = strTownState.rfind(",") # Checking if there is a second comma
-    if (iSecondComma == -1):
-        strState = strTownState
-        strTown = ""
-    else:
-        strState = strTownState[iSecondComma+2:]
-        strTown = strTownState[:iSecondComma]
+        # Sleep a little before the next recursive function call
+        time.sleep(.5)
 
-    # Storing into a list
-    author = [strAuthorTitle, dateTimeObject.date().year, dateTimeObject.date().month, dateTimeObject.date().day, strCountry, strState, strTown]
-    authors.append(author)
-
-    # Printing the stored item out
-    print(author)
-
+# Start program by calling function with:
+# 1) Subreddit specified
+# 2) The type of data required (comment or submission)
+extract_reddit_data(subreddit="2007scape",type="submission")
